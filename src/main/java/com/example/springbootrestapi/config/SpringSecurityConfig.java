3c192d7120df78b245cb06320ef57f5ed3c8a325
Rasmus3liasson <ras.muse@hotmail.com>
2023-11-27 12:33:16 +0100

    added retry for my geomaps function

--- src/main/java/com/example/springbootrestapi/config/SpringSecurityConfig.java
+++ src/main/java/com/example/springbootrestapi/config/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +23,7 @@ import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
+@EnableRetry
 public class SpringSecurityConfig {
 
     @Bean
--- src/main/java/com/example/springbootrestapi/geomaps/GeoMaps.java
+++ src/main/java/com/example/springbootrestapi/geomaps/GeoMaps.java
@@ -1,16 +1,25 @@
 package com.example.springbootrestapi.geomaps;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.sql.SQLException;
 import java.util.Arrays;
 
 
 @Service
 public class GeoMaps {
+    private final Logger logger = LoggerFactory.getLogger(GeoMaps.class);
     private final String url = "https://geocode.maps.co/reverse";
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Retryable(retryFor = {HttpClientErrorException.class, HttpServerErrorException.class}, maxAttempts = 3, backoff = @Backoff(delay = 300))
     public AddressInfo getLocation(double latitude, double longitude) {
         // Format data
         String apiUrl = String.format("%s?format=json&lat=%s&lon=%s", url, latitude, longitude);
@@ -22,8 +31,7 @@ public class GeoMaps {
                 Arrays.stream(res.display_name().split(",")).toArray(),
                 res.address()
         );
-
-
+        logger.info("Location data retrieval successful!");
         return addressData;
     }
 }
